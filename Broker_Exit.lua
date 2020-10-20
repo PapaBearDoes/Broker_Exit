1a68758fb60a17a7f3626e8a8d4e9097a4076ee8
PapaBearDoes <73200436+PapaBearDoes@users.noreply.github.com>
2020-10-20 16:09:19 -0600

    Initial Commit - Files are from WoW 7.0
    Initial Commit for Broker_Exit.
    
    Files are from the addon I made, last updated for WoW 7.0

---
+++ Broker_Exit.lua
@@ -0,0 +1,23 @@
+local ldb = LibStub:GetLibrary("LibDataBroker-1.1");
+local dataobj = ldb:NewDataObject("Broker_Exit", {
+	type = "data source",
+	icon = "Interface\\Icons\\inv_pandarenserpentmount_yellow",
+	OnClick = function(self, button)
+		if button == "LeftButton" then
+			if IsShiftKeyDown() then
+				Logout(); -- Logout
+			elseif IsControlKeyDown() then
+				ForceQuit(); -- Hard Exit Game
+			elseif IsAltKeyDown() then
+				-- Do Nothing
+			else
+				ReloadUI(); --Reload the UI
+			end
+		end
+	end,
+	OnTooltipShow = function(tooltip)
+		tooltip:AddDoubleLine("To |cff1eff00RELOAD|r:", "|cff1eff00Left Click|r Icon.");
+		tooltip:AddDoubleLine("To |cffff8000LOGOUT|r:", "|cffff8000<SHIFT> + Left Click|r Icon.");
+		tooltip:AddDoubleLine("To |cffa335eeEXIT|r:", "|cffa335ee<CONTROL> + Left Click|r Icon.");
+	end
+});
